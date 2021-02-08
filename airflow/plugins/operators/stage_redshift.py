157d9a0983a6e75cc5ce2301b360ca518321e8c7
morganpsell@gmail.com <morganpsell@gmail.com>
2021-02-08 17:58:46 +0000

    started building the stage_redshift operator

--- airflow/plugins/operators/stage_redshift.py
+++ airflow/plugins/operators/stage_redshift.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
@@ -10,15 +11,38 @@ class StageToRedshiftOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # redshift_conn_id=your-connection-name
+                 redshift_conn_id="redshift",
+                 aws_credentials_id="aws_credentials",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 delimiter=",",
+                 ignore_headers=1,
                  *args, **kwargs):
+               
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id,
+        self.table = table,
+        self.aws_credentials_id = aws_credentials_id,
+        self.s3_bucket = s3_bucket,
+        self.s3_key = s3_key,
+        self.delimiter = delimeter,
+        self.ignore_headers = ignore_headers
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run("DELETE FROM {}".format(self.table))
+        
+        self.log.info("Copying data from S3 to Redshift")
+        rendered_key = self.s3_key.format(**context)
 
 
 
