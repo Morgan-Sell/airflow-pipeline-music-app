033055f771a86162e916f509dccf56a11e3ff657
root <morganpsell@gmail.com>
2021-02-10 20:49:17 +0000

    started building LoadDimensionOperator

--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -5,7 +5,9 @@ from airflow.utils.decorators import apply_defaults
 from airflow.plugins.helpers import sql_queries
 
 class LoadDimensionOperator(BaseOperator):
-    
+    truncate_sql = """
+    TRUNCATE TABLE {destination_table};
+    """    
     ui_color = '#80BD9E'
 
     @apply_defaults
@@ -13,12 +15,30 @@ class LoadDimensionOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 redshift_conn_id = "redshift",
+                 origin_table = "",
+                 destination_table = "",
+                 truncate = True,
+                 load_sql_query = "",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.destination_table = destination_table
+        self.truncate = truncate
+        self.load_sql_query = load_sql_query
+        
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info('Creating dimensions tables')
+        # If truncate is True, add appropicate SQL command.
+        if self.truncate:
+            dimensions_sql = LoadDimensionOperator.truncate_sql + LoadDimensionOperator.load_sql_query
+        else:
+            dimensions_sql = LoadDimensionOperator.load_sql_query
+        
+        redshift.run(dimensions_sql)
